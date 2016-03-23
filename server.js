ef44ddcd97c4604ace0ff9eb3b61854956b711eb
trojansaga <trojansaga@naver.com>
2016-03-23 21:25:19 +0900

    # test commit

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World! from haproxy amazing!!!');
 });
 
+app.get('/test', function (req, res) {
+    res.send('this is test');
+})
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-  console.log('this is first step');
+    console.log('Example app listening on port 3000s!');
+    console.log('this is first step');
+    console.log('this is from haproxy');
 });
